8c4c14b9d3bcf3b2a331d5664298313f471e13d0
Rebbit13 <errb13@gmail.com>
2022-08-04 19:25:09 +0400

    add error handling middleware

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from starlette.middleware.base import BaseHTTPMiddleware
 from tortoise.contrib.fastapi import register_tortoise
 
 import settings
 from backend.app import user_router, todo_router
+from backend.app.midlewares import error_handling
 
 
 def register_db(app: FastAPI) -> FastAPI:
@@ -20,6 +22,7 @@ def create_app() -> FastAPI:
     app = FastAPI(
         debug=bool(settings.DEBUG)
     )
+    app.add_middleware(BaseHTTPMiddleware, dispatch=error_handling)
     app.include_router(user_router)
     app.include_router(todo_router)
     app = register_db(app)
